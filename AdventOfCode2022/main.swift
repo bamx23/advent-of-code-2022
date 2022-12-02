52eabea26b77f563bd1ef5f349ddc4a2075053ba
Nikolay Volosatov <bamx23@meta.com>
2022-12-02 10:04:10 +0000

    Day 02

--- AdventOfCode2022/main.swift
+++ AdventOfCode2022/main.swift
@@ -49,6 +49,137 @@ func task01_2(_ input: TaskInput) {
     print("T01_2: \(max3)")
 }
 
+// MARK: - Day 02
+
+extension TaskInput {
+    enum Task02 {
+        enum Figure {
+            case rock
+            case paper
+            case scissors
+        }
+    }
+    func task02() -> [(Task02.Figure, Task02.Figure)] {
+        readInput("02")
+            .split(separator: "\n")
+            .map { line in
+                let pair = line.split(separator: " ")
+                return (.fromTheir(pair[0]), .fromYour(pair[1]))
+            }
+    }
+}
+
+extension TaskInput.Task02.Figure {
+    var score: Int {
+        switch self {
+        case .rock:
+            return 1
+        case .paper:
+            return 2
+        case .scissors:
+            return 3
+        }
+    }
+    
+    static func fromTheir(_ str: Substring) -> Self {
+        switch str {
+        case "A":
+            return .rock
+        case "B":
+            return .paper
+        case "C":
+            return .scissors
+        default:
+            fatalError()
+        }
+    }
+    
+    static func fromYour(_ str: Substring) -> Self {
+        switch str {
+        case "X":
+            return .rock
+        case "Y":
+            return .paper
+        case "Z":
+            return .scissors
+        default:
+            fatalError()
+        }
+    }
+    
+    enum Result: Int {
+        case win = 6
+        case lose = 0
+        case draw = 3
+    }
+    
+    func vs(_ other: Self) -> Result {
+        switch (self, other) {
+        case (.rock, .paper):
+            return .lose
+        case (.rock, .scissors):
+            return .win
+        case (.paper, .scissors):
+            return .lose
+        case (.paper, .rock):
+            return .win
+        case (.scissors, .rock):
+            return .lose
+        case (.scissors, .paper):
+            return .win
+        default:
+            return .draw
+        }
+    }
+    
+    func how(_ result: Result) -> Self {
+        switch (self, result) {
+        case (_, .draw):
+            return self
+        case (.rock, .win):
+            return .paper
+        case (.rock, .lose):
+            return .scissors
+        case (.paper, .win):
+            return .scissors
+        case (.paper, .lose):
+            return .rock
+        case (.scissors, .win):
+            return .rock
+        case (.scissors, .lose):
+            return .paper
+        }
+    }
+    
+    var correctParsing: Result {
+        switch self {
+        case .rock:
+            return .lose
+        case .paper:
+            return .draw
+        case .scissors:
+            return .win
+        }
+    }
+}
+
+func task02_1(_ input: TaskInput) {
+    let plan = input.task02()
+    let score = plan
+        .map { (a, b) in b.vs(a).rawValue + b.score }
+        .reduce(0, +)
+    print("T02_1: \(score)")
+}
+
+func task02_2(_ input: TaskInput) {
+    let plan = input.task02()
+    let score = plan
+        .map { (a, b) in (a, b.correctParsing) }
+        .map { (a, b) in b.rawValue + a.how(b).score }
+        .reduce(0, +)
+    print("T02_2: \(score)")
+}
+
 // MARK: - Main
 
 let inputs = [
@@ -59,8 +190,11 @@ let inputs = [
 for input in inputs {
     print("Run for \(input.prefix)")
     let start = Date()
-    task01_1(input)
-    task01_2(input)
+//    task01_1(input)
+//    task01_2(input)
+  
+    task02_1(input)
+    task02_2(input)
 
     print("Time: \(String(format: "%0.4f", -start.timeIntervalSinceNow))")
 }
