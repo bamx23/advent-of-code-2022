994eb882e663ad9cf89a659187c03c8ad3817571
Nikolay Volosatov <bamx23@meta.com>
2022-12-07 09:08:55 +0000

    Day 07

--- AdventOfCode2022/main.swift
+++ AdventOfCode2022/main.swift
@@ -15,7 +15,6 @@ struct TaskInput {
         guard let url = Bundle.main.url(forResource: name, withExtension: "txt", subdirectory: "input")
         else {
             fatalError("Not found: input/\(name).txt")
-
         }
         return try! String(contentsOf: url)
     }
@@ -406,6 +405,149 @@ func task06_2(_ input: TaskInput) {
     print("T06_2: \(signals)")
 }
 
+// MARK: - Day 07
+
+extension TaskInput {
+    enum Task07 {
+        class Dir {
+            let name: String
+            var nodes: [Node] = []
+
+            init(name: String) {
+                self.name = name
+            }
+        }
+        struct File {
+            var name: String
+            var size: Int
+        }
+        enum Node {
+            case dir(Dir)
+            case file(File)
+        }
+
+        static func parse(_ cmds: [Substring]) -> Dir
+        {
+            if cmds.isEmpty || cmds[0] != "$ cd /" {
+                fatalError()
+            }
+            let rootDir = Dir(name: "")
+            var dirStack = [rootDir]
+            var idx = 1
+            while idx < cmds.count {
+                let cmd = cmds[idx]
+
+                // ls command
+                if cmd == "$ ls" {
+                    var nodes = [Node]()
+                    idx += 1
+                    while idx < cmds.count && !cmds[idx].hasPrefix("$") {
+                        let parts = cmds[idx].split(separator: " ")
+                        let name = String(parts[1])
+                        if parts[0] == "dir" {
+                            nodes.append(.dir(.init(name: name)))
+                        } else {
+                            nodes.append(.file(.init(name: name, size: Int(parts[0])!)))
+                        }
+                        idx += 1
+                    }
+                    dirStack.last!.nodes = nodes
+                    continue
+                }
+
+                // cd command
+                if cmd.hasPrefix("$ cd") {
+                    let name = cmd.split(separator: " ").last!
+
+                    if name == ".." {
+                        dirStack.removeLast()
+                    } else {
+                        let nextDir = dirStack.last!.nodes
+                            .compactMap { if case .dir(let dir) = $0 { return dir } else { return nil } }
+                            .first(where: { $0.name == name })!
+                        dirStack.append(nextDir)
+                    }
+                    idx += 1
+                }
+            }
+            return rootDir
+        }
+
+        static func dirSizes(_ dir: Dir) -> [String: Int] {
+            var result = [String: Int]()
+
+            func rec(_ dir: Dir, path: String) -> Int {
+                var thisSize = 0
+                for node in dir.nodes {
+                    switch node {
+                    case .file(let file):
+                        thisSize += file.size
+                    case .dir(let dir):
+                        thisSize += rec(dir, path: "\(path)/\(dir.name)")
+                    }
+                }
+                result[path] = thisSize
+                return thisSize
+            }
+
+            _ = rec(dir, path: "")
+            return result
+        }
+
+        static func printTree(_ dir: Dir) {
+            func rec(_ dir: Dir, prefix: String) {
+                for node in dir.nodes.sorted(by: { (a,b) in a.name < b.name }) {
+                    switch node {
+                    case .file(let file):
+                        print("\(prefix)- \(file.name) (file, size=\(file.size))")
+                    case .dir(let subdir):
+                        print("\(prefix)- \(subdir.name) (dir)")
+                        rec(subdir, prefix: prefix + "  ")
+                    }
+                }
+            }
+            print("- / (dir)")
+            rec(dir, prefix: "  ")
+        }
+    }
+
+    func task07() -> Task07.Dir {
+        Task07.parse(readInput("07").split(separator: "\n"))
+    }
+}
+
+extension TaskInput.Task07.Node {
+    var name: String {
+        switch self {
+        case .file(let file):
+            return file.name
+        case .dir(let dir):
+            return dir.name
+        }
+    }
+}
+
+func task07_1(_ input: TaskInput) {
+    let root = input.task07()
+//    TaskInput.Task07.printTree(root)
+    let sizes = TaskInput.Task07.dirSizes(root)
+    let result = sizes.values
+        .filter { $0 < 100000 }
+        .reduce(0, +)
+    print("T07_1: \(result)")
+}
+
+func task07_2(_ input: TaskInput) {
+    let root = input.task07()
+    let dirSizes = TaskInput.Task07.dirSizes(root)
+    let remainSize = 70000000 - dirSizes[""]!
+    let toRemove = 30000000 - remainSize
+    let removing = dirSizes.values
+        .sorted()
+        .first(where: { $0 >= toRemove })!
+    print("T07_2: \(removing)")
+}
+
 // MARK: - Main
 
 let inputs = [
@@ -431,8 +573,11 @@ for input in inputs {
 //    task05_1(input)
 //    task05_2(input)
 
-    task06_1(input)
-    task06_2(input)
+//    task06_1(input)
+//    task06_2(input)
+
+    task07_1(input)
+    task07_2(input)
 
     print("Time: \(String(format: "%0.4f", -start.timeIntervalSinceNow))")
 }
