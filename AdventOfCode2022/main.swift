f86dd9a5e8fb6010397c7f6aa4984b50117a2b7d
Nikolay Volosatov <code@bamx23.com>
2022-12-14 19:24:58 +0000

    Day 14

--- AdventOfCode2022/main.swift
+++ AdventOfCode2022/main.swift
@@ -898,6 +898,160 @@ func task10_2(_ input: TaskInput) {
     print("T10_2:\n\(result)")
 }
 
+// MARK: - Day 14
+
+extension TaskInput {
+    enum Task14 {
+        typealias Pos = (x: Int, y: Int)
+        typealias Path = [Pos]
+        
+        enum Tile {
+            case air
+            case rock
+            case sand
+        }
+    }
+
+    func task14() -> [Task14.Path] {
+        readInput("14")
+            .split(separator: "\n")
+            .map { line in
+                line.split(separator: ">")
+                    .map { $0.hasSuffix(" -") ? $0.dropLast(2) : $0 }
+                    .map { $0.hasPrefix(" ") ? $0.dropFirst() : $0 }
+                    .map { str -> Task14.Pos in
+                        let pair = str.split(separator: ",")
+                        return (x: Int(pair[0])!, y: Int(pair[1])!)
+                    }
+            }
+    }
+}
+
+func task14_1(_ input: TaskInput) {
+    let paths = input.task14()
+    
+    let minX = paths.map { $0.map(\.x).min()! }.min()!
+    let maxX = paths.map { $0.map(\.x).max()! }.max()!
+    let minY = paths.map { $0.map(\.y).min()! }.min()!
+    let maxY = paths.map { $0.map(\.y).max()! }.max()!
+    let (h, w) = (maxY - minY + 1, maxX - minX + 1)
+    let gap = 50
+    
+    func mX(_ pos: TaskInput.Task14.Pos) -> Int { pos.x - minX + gap }
+    func mY(_ pos: TaskInput.Task14.Pos) -> Int { pos.y - minY + gap }
+    
+    var map = [[TaskInput.Task14.Tile]](repeating: [TaskInput.Task14.Tile](repeating: .air, count: w + gap * 2), count: h + gap * 2)
+    for path in paths {
+        for (a, b) in zip(path.dropLast(), path.dropFirst()) {
+            if a.x == b.x {
+                for y in min(a.y, b.y)...max(a.y, b.y) {
+                    map[y - minY + gap][a.x - minX + gap] = .rock
+                }
+            } else if a.y == b.y {
+                for x in min(a.x, b.x)...max(a.x, b.x) {
+                    map[a.y - minY + gap][x - minX + gap] = .rock
+                }
+            } else {
+                fatalError()
+            }
+        }
+    }
+    
+    let sandStart: TaskInput.Task14.Pos = (500, 0)
+    while true {
+        var sand = sandStart
+        var isInfinite = false
+        while true {
+            if sand.y > maxY {
+                isInfinite = true
+                break
+            }
+            if map[mY(sand) + 1][mX(sand)] == .air {
+                sand.y += 1
+            } else if map[mY(sand) + 1][mX(sand) - 1] == .air {
+                sand.y += 1
+                sand.x -= 1
+            } else if map[mY(sand) + 1][mX(sand) + 1] == .air {
+                sand.y += 1
+                sand.x += 1
+            } else {
+                map[mY(sand)][mX(sand)] = .sand
+                break
+            }
+        }
+        if isInfinite {
+            break
+        }
+    }
+    
+    let count = map
+        .map { line in line.filter { $0 == .sand }.count }
+        .reduce(0, +)
+    print("T14_1: \(count)")
+}
+
+func task14_2(_ input: TaskInput) {
+    let paths = input.task14()
+    
+    let minX = paths.map { $0.map(\.x).min()! }.min()!
+    let maxX = paths.map { $0.map(\.x).max()! }.max()!
+    let minY = paths.map { $0.map(\.y).min()! }.min()!
+    let maxY = paths.map { $0.map(\.y).max()! }.max()!
+    let (h, w) = (maxY - minY + 1, maxX - minX + 1)
+    let gap = 200
+    
+    func mX(_ pos: TaskInput.Task14.Pos) -> Int { pos.x - minX + gap }
+    func mY(_ pos: TaskInput.Task14.Pos) -> Int { pos.y - minY + gap }
+    
+    var map = [[TaskInput.Task14.Tile]](repeating: [TaskInput.Task14.Tile](repeating: .air, count: w + gap * 2), count: h + gap * 2)
+    for path in paths {
+        for (a, b) in zip(path.dropLast(), path.dropFirst()) {
+            if a.x == b.x {
+                for y in min(a.y, b.y)...max(a.y, b.y) {
+                    map[y - minY + gap][a.x - minX + gap] = .rock
+                }
+            } else if a.y == b.y {
+                for x in min(a.x, b.x)...max(a.x, b.x) {
+                    map[a.y - minY + gap][x - minX + gap] = .rock
+                }
+            } else {
+                fatalError()
+            }
+        }
+    }
+    
+    let sandStart: TaskInput.Task14.Pos = (500, 0)
+    while true {
+        var sand = sandStart
+        if map[mY(sand)][mX(sand)] == .sand {
+            break
+        }
+        while true {
+            if sand.y == maxY + 1 {
+                map[mY(sand)][mX(sand)] = .sand
+                break
+            }
+            if map[mY(sand) + 1][mX(sand)] == .air {
+                sand.y += 1
+            } else if map[mY(sand) + 1][mX(sand) - 1] == .air {
+                sand.y += 1
+                sand.x -= 1
+            } else if map[mY(sand) + 1][mX(sand) + 1] == .air {
+                sand.y += 1
+                sand.x += 1
+            } else {
+                map[mY(sand)][mX(sand)] = .sand
+                break
+            }
+        }
+    }
+    
+    let count = map
+        .map { line in line.filter { $0 == .sand }.count }
+        .reduce(0, +)
+    print("T14_2: \(count)")
+}
+
 // MARK: - Main
 
 let inputs = [
@@ -935,8 +1089,11 @@ for input in inputs {
 //    task09_1(input)
 //    task09_2(input)
     
-    task10_1(input)
-    task10_2(input)
+//    task10_1(input)
+//    task10_2(input)
+    
+    task14_1(input)
+    task14_2(input)
 
     print("Time: \(String(format: "%0.4f", -start.timeIntervalSinceNow))")
 }
